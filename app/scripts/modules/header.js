3c75190b783062549d520dde03bd33b2685dd3d9
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-06-13 16:22:52 -0400

    click toggle setup to show and hide content on tablet+

--- app/scripts/modules/header.js
+++ app/scripts/modules/header.js
@@ -5,11 +5,33 @@ import React from 'react';
 
 export default class Header extends React.Component { 
 
+
+	// All Click Functions Declared Here
+	handle_click(event) { 
+		event.preventDefault();
+		var siblings = document.querySelectorAll(".active");
+		[].forEach.call(siblings, function(el) {
+   			 el.classList.remove("active");
+		});
+		var currentId = event.target.getAttribute('href')
+		document.getElementById(currentId).setAttribute('class', 'active');
+	}  	
+
   render() {
     return (
       <header>
-        <h1>Paige Keenan</h1>
-        <p>Front End Developer</p>
+        <h1>Erin Stevens</h1>
+        <p>Art Director</p>
+        <nav>
+	        <a  href='new-work'
+	        	onClick={this.handle_click.bind(this)}>
+	        	New Work
+	        </a>
+	        <a  href='old-work' 
+	        	onClick={this.handle_click.bind(this)}>
+	        	Old Work
+	        </a>
+	    </nav>    
       </header>
     );
   }
