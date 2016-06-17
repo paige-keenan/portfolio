ea5420b9a890477c734390d1f4b646dbc353478f
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2016-06-17 13:38:24 -0500

    leave demos closed by default on mobile

---
+++ app/scripts/modules/demos.js
@@ -0,0 +1,45 @@
+// New Work component
+// =============================
+
+import React    from 'react';
+import DropDown from './drop-down.js'
+import Project  from './project.js'
+
+export default class NewWork extends React.Component { 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Demos',
+      isOpen: false,
+      id: 'demos'
+    }
+  }	
+
+  // Toggle Menu Function
+  toggle_menu(event) {
+    this.setState({isOpen: !this.state.isOpen});
+ 
+  }
+
+  // All Click Functions Declared Here
+  handle_click(event) { 
+    this.toggle_menu(this);
+  }    
+
+
+  render() {
+    return (
+    	<section
+        id={this.state.id}
+        className='active'
+    		data-is-active={this.state.isOpen ? true : false}>
+    		<DropDown 
+    			data={this.state.title}   		
+    			clickEvent={this.handle_click.bind(this)}/>
+    		<Project
+    			data={this.props.data}/>
+    	</section>	
+    );
+  }
+}
